69411d33b4fbef7e809362e33d56e1f0e87d089f
evans123456 <evanpersie3@gmail.com>
2019-11-03 22:27:53 +0300

    fake posts showing

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import CustomLayout from './containers/Layout'
+import CustomLayout from './containers/Layout';
+import ArticleList from './containers/ArticleListView';
 
 function App() {
   return (
     <div className="App">
       hello
         <CustomLayout>
-          
+            <ArticleList/>
         </CustomLayout>
     </div>
   );
---
+++ frontend/gui/src/containers/ArticleListView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Article from '../components/Article';
+
+// this container is for rendering out all articles
+class ArticleList extends React.Component {
+    render(){
+        return(
+            <Article/>
+        )
+    }
+}
+
+export default ArticleList
